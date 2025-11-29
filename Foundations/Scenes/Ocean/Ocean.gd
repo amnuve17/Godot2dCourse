9cbfca688259e38a0326c185515f2c62ab44159e
amnuve17 <amnuve@outlook.it>
2025-11-29 21:41:23 +0100

    Completed Section 3: Foundations
    Completed: Lesson 13 - Translate
    Completed: Lesson 14 - Look At
    Completed: Lesson 15 - Move forward
    Completed: Lesson 16 - Transform Vector2 and Move local
    Completed: Lesson 17 - Rotate

--- Foundations/Scenes/Ocean/Ocean.gd
+++ Foundations/Scenes/Ocean/Ocean.gd
@@ -3,17 +3,27 @@ extends Node2D
 @onready var plane: Sprite2D = $Plane;
 @onready var helicopter: Sprite2D = %Helicopter;
 
+var _target: Vector2 = Vector2.ZERO;
+const ROT_SPEED: float = PI / 2;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#plane.rotation = helicopter.global_rotation;
-	helicopter.global_rotation = plane.global_rotation;
+	#helicopter.global_rotation = plane.global_rotation;
+	pass
+	
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("SetActive"):
+		_target = get_global_mouse_position();
+		plane.look_at(_target);
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#plane.position.x -= 50.0 * delta;
-	plane.rotation_degrees += 30.0 * delta;
+	#plane.rotation_degrees += 30.0 * delta;
 	#helicopter.position.x -= 50.0 / helicopter.global_scale.x * delta;
 	#print("helicopter.position: ", helicopter.position);
 	#print("helicopter.global_position: ", helicopter.global_position);
@@ -24,4 +34,23 @@ func _process(delta: float) -> void:
 	#or
 	#print("helicopter.rotation: ", helicopter.rotation_degrees);
 	#print("helicopter.global_rotation: ", helicopter.global_rotation_degrees);
+	
+	#helicopter.position.x += 30.0 * delta;
+	#helicopter.position.y += 50.0 * delta;
+	#helicopter.translate(Vector2(30.0, 50.0) * delta);
+	#helicopter.global_translate(Vector2(30.0, 50.0) * delta);
+	
+	#Use the last method is is far less expensive
+	#plane.position = plane.position.move_toward(_target, 50.0 * delta);
+	#plane.position += plane.transform.x.normalized() * 50.0 * delta;
+	
+	if(Input.is_action_pressed("ui_up")):
+		plane.move_local_x(50.0 * delta);
+	
+	if (Input.is_action_pressed("ui_left")):
+		plane.rotate(-ROT_SPEED * delta);
+			
+	if (Input.is_action_pressed("ui_right")):
+		plane.rotate(ROT_SPEED * delta);	
+	
 	pass
